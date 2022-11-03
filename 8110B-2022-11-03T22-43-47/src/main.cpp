85600f2b52f244c6e6975e8eeaab85aa0d47c4a9
mastery55295 <93741731+mastery55295@users.noreply.github.com>
2022-11-03 17:44:30 -0500

    add everything

---
+++ 8110B-2022-11-03T22-43-47/src/main.cpp
@@ -0,0 +1,136 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Motor13              motor         13              
+// LimitSwitchA         limit         A               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+
+void cataReset() {
+  while(!Limit1) {
+    Cata.spin(fwd);
+  }
+  Cata.stop(hold);
+}
+void cataShoot() {
+  while(Limit1) {
+    Cata.spin(fwd);
+  }
+  while(!Limit1) {
+    return;
+  }
+  Cata.stop(hold);
+}
+
+void cataRest() {
+     
+}
+
+void usercontrol(void) {
+  Controller1.ButtonR1.pressed(cataShoot);
+  // User control code here, inside the loop
+  while (1) {
+
+    
+    /*if(!Limit1.pressing())
+    {
+      if(Controller1.ButtonR2.pressing())
+      {
+        Cata.spin(forward);
+      }
+    }
+    if(Limit1.pressing())
+    {
+      if(Controller1.ButtonX.pressing())
+      {
+        Cata.stop();
+      }
+    }
+    if(!Controller1.ButtonR2.pressing())
+    {
+      Cata.stop();
+    }*/
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
